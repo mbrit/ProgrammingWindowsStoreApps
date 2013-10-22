f26e435c8c1c6e393aaaecd3fc2417f002e428a3
Matt Baxter-Reynolds (@mbrit) <mbrit@mbrit.com>
2013-10-22 18:13:49 +0100

    Chapter 9

---
+++ Chapter09/StreetFoo.Client/StreetFoo.Client.UI/Objects/MvvmAwareControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace StreetFoo.Client.UI
+{
+    public class MvvmAwareControl : UserControl, IViewModelHost
+    {
+        public MvvmAwareControl()
+        {
+        }
+
+        Task IViewModelHost.ShowAlertAsync(ErrorBucket errors)
+        {
+            return PageExtender.ShowAlertAsync(this, errors).AsTask();
+        }
+
+        Task IViewModelHost.ShowAlertAsync(string message)
+        {
+            return PageExtender.ShowAlertAsync(this, message).AsTask();
+        }
+
+        public void ShowView(Type viewModelInterfaceType, object parameter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HideAppBar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Chapter09/StreetFoo.Client/StreetFoo.Client.UI/Objects/PageExtender.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using TinyIoC;
+using Windows.Foundation;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+
+namespace StreetFoo.Client.UI
+{
+    // extension methods for presenting MessageDialog instances...
+    internal static class PageExtender
+    {
+        internal static IAsyncOperation<IUICommand> ShowAlertAsync(this IViewModelHost page, ErrorBucket errors)
+        {
+            return ShowAlertAsync(page, errors.GetErrorsAsString());
+        }
+
+        internal static IAsyncOperation<IUICommand> ShowAlertAsync(this IViewModelHost page, string message)
+        {
+            // show...
+            MessageDialog dialog = new MessageDialog(message != null ? message : string.Empty);
+            return dialog.ShowAsync();
+        }
+
+        internal static void InitializeViewModel(this IViewModelHost host, IViewModel model = null)
+        {
+            // if we don't get given a model?
+            if (model == null)
+            {
+                var attr = (ViewModelAttribute)host.GetType().GetTypeInfo().GetCustomAttribute<ViewModelAttribute>();
+                if (attr != null)
+                    model = (IViewModel)TinyIoCContainer.Current.Resolve(attr.ViewModelInterfaceType);
+                else
+                    throw new InvalidOperationException(string.Format("Page '{0}' is not decorated with ViewModelAttribute.", host));
+            }
+
+            // setup...
+            model.Initialize((IViewModelHost)host);
+            ((FrameworkElement)host).DataContext = model;
+        }
+
+        internal static IViewModel GetModel(this IViewModelHost page)
+        {
+            return ((Control)page).DataContext as IViewModel;
+        }
+    }
+}
