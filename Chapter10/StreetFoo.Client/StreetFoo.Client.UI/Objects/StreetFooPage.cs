0760b271f7fcc6b69feccd93b6fe4d278454a890
Matt Baxter-Reynolds (@mbrit) <mbrit@mbrit.com>
2013-11-01 10:45:10 +0000

    Chapter 10

---
+++ Chapter10/StreetFoo.Client/StreetFoo.Client.UI/Objects/StreetFooPage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.System;
+using Windows.UI.Xaml;
+
+namespace StreetFoo.Client.UI
+{
+    public class StreetFooPage : Page, IViewModelHost
+    {
+        Task IViewModelHost.ShowAlertAsync(string message)
+        {
+            return PageExtender.ShowAlertAsync(this, message).AsTask();
+        }
+
+        Task IViewModelHost.ShowAlertAsync(ErrorBucket errors)
+        {
+            return PageExtender.ShowAlertAsync(this, errors).AsTask();
+        }
+
+        public void ShowView(Type viewModelType, object parameter = null)
+        {
+            this.Frame.ShowView(viewModelType, parameter);
+        }
+
+        protected override void OnNavigatedTo(Windows.UI.Xaml.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            // ok...
+            this.GetModel().Activated(e.Parameter);
+        }
+
+        public void ShowAppBar()
+        {
+            if (this.BottomAppBar != null)
+                this.BottomAppBar.IsOpen = true;
+        }
+
+        public void HideAppBar()
+        {
+            if (this.BottomAppBar != null)
+                this.BottomAppBar.IsOpen = false;
+        }
+
+        protected override void OnKeyUp(KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.F1)
+                App.ShowHelp();
+            else
+                base.OnKeyUp(e);
+        }
+
+        public void GoBack()
+        {
+            if (this.Frame != null && this.Frame.CanGoBack)
+                this.Frame.GoBack();
+        }
+
+        protected virtual void GoBack(object sender, RoutedEventArgs e)
+        {
+            this.GoBack();
+        }
+    }
+}
---
+++ Chapter10/StreetFoo.Client/StreetFoo.Client/Model/Base/ViewModelSingleton.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows.Input;
+
+namespace StreetFoo.Client
+{
+    public abstract class ViewModelSingleton<T> : ViewModel, IViewModelSingleton<T>
+        where T : ModelItem
+    {
+        // save and cancel commands
+        public ICommand SaveCommand { get { return this.GetValue<ICommand>(); } private set { this.SetValue(value); } }
+        public ICommand CancelCommand { get { return this.GetValue<ICommand>(); } private set { this.SetValue(value); } }
+
+        // holds the base item that we're mapped to...
+        private T _item;
+
+        protected ViewModelSingleton()
+        {
+            this.SaveCommand = new DelegateCommand((args) => Save());
+            this.CancelCommand = new DelegateCommand((args) => Cancel());
+        }
+
+        public T Item
+        {
+            get
+            {
+                return _item;
+            }
+            set
+            {
+                _item = value;
+                this.OnPropertyChanged();
+
+                // reload...
+                this.ItemChanged();
+            }
+        }
+
+        protected virtual void ItemChanged()
+        {
+            // no-op...
+        }
+
+        public override void Activated(object args)
+        {
+            base.Activated(args);
+
+            // check...
+            if (args == null)
+                throw new InvalidOperationException("An item was not supplied.");
+
+            // check...
+            if (args is IList)
+            {
+                var list = (IList)args;
+                if (list.Count > 0)
+                    args = (T)list[0];
+            }
+
+            // match?
+            if(!(typeof(T).GetTypeInfo().IsAssignableFrom(args.GetType().GetTypeInfo())))
+            {
+                throw new InvalidOperationException(string.Format("An item of type '{0}' was supplied, but an item of type '{1}' was required.",
+                    args.GetType(), typeof(T)));
+            }
+
+            // are our arguments initializing our item?
+            this.Item = (T)args;
+        }
+
+        protected virtual void Save()
+        {
+        }
+
+        protected virtual void Cancel()
+        {
+            // go back...
+            this.Host.GoBack();
+        }
+    }
+}
