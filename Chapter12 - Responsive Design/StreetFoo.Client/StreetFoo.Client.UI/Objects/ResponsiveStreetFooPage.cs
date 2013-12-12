ceb22fcf1a6ba87e0a252caa2b2bc7a94fe230cf
Iris Classon <iris@irisclasson.com>
2013-12-12 23:57:09 +0100

    Layout awerness class

---
+++ Chapter12 - Responsive Design/StreetFoo.Client/StreetFoo.Client.UI/Objects/ResponsiveStreetFooPage.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml;
+
+namespace StreetFoo.Client.UI.Objects
+{
+    public class ResponsiveStreetFooPage : StreetFooPage
+    {
+        public ResponsiveStreetFooPage()
+        {
+            SizeChanged += OnSizeChanged;
+        }
+        private const double SmallMode = 320;
+        private const double MediumMode = 500;
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width <= SmallMode)
+            {
+                VisualStateManager.GoToState(this, "Small", true);
+            }
+            else if (e.NewSize.Width <= MediumMode && e.NewSize.Width > SmallMode)
+            {
+                VisualStateManager.GoToState(this, "Medium", true);
+            }
+            else if (e.NewSize.Width <= MediumMode)
+            {
+                VisualStateManager.GoToState(this, "Default", true);
+            }
+        }
+    }
+}
