46b5aee03b663f3f61ab4e88e473cc59d4dd8c5e
Matt Baxter-Reynolds (@mbrit) <mbrit@mbrit.com>
2013-12-19 22:08:16 +0000

    Responsive view for edit paeg

--- Chapter12 - Responsive Design/StreetFoo.Client/StreetFoo.Client.UI/EditReportPage.xaml.cs
+++ Chapter12 - Responsive Design/StreetFoo.Client/StreetFoo.Client.UI/EditReportPage.xaml.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -20,12 +22,35 @@ namespace StreetFoo.Client.UI
     /// A basic page that provides characteristics common to most applications.
     /// </summary>
     [ViewModel(typeof(IEditReportPageViewModel))]
-    public sealed partial class EditReportPage : StreetFooPage
+    public sealed partial class EditReportPage : ResponsiveStreetFooPage
     {
         public EditReportPage()
         {
             this.InitializeComponent();
             this.InitializeViewModel();
+        }
+
+        private async void HandleMoreButton(object sender, RoutedEventArgs e)
+        {
+            var popup = new PopupMenu();
+            popup.Commands.Add(new UICommand("Take Picture", async (args) =>
+            {
+
+                //// try and unsnap, then show...
+                //if (ApplicationView.TryUnsnap())
+                //    this.ViewModel.TakePhotoCommand.Execute(null);
+
+                await this.ShowAlertAsync("Make the app full screen to use the camera.");
+
+            }));
+            popup.Commands.Add(new UICommand("Capture Location", (args) => 
+            {
+                var viewModel = (IEditReportPageViewModel)this.GetViewModel();
+                viewModel.CaptureLocationCommand.Execute(null);
+            }));
+
+            // show...
+            await popup.ShowAsync(((FrameworkElement)sender).GetPointForContextMenu());
         }
     }
 }
--- Chapter12 - Responsive Design/StreetFoo.Client/StreetFoo.Client.UI/RegisterPage.xaml.cs
+++ Chapter12 - Responsive Design/StreetFoo.Client/StreetFoo.Client.UI/RegisterPage.xaml.cs
@@ -12,7 +12,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
-using StreetFoo.Client.UI.Objects;
 
 namespace StreetFoo.Client.UI
 {
